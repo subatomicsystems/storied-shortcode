9bd95d721b4f6ec55bb47897a45121330c87fff8
Gilles De Mey <gilles.de.mey@gmail.com>
2015-07-02 19:16:08 +0200

    Added minification options

---
+++ test/with_options/with_options.js
@@ -0,0 +1,22 @@
+'use strict'
+
+var parse   = require('../../lib/parse')
+var should  = require('chai').should()
+var cheerio = require('cheerio')
+
+describe('With Options', function() {
+
+  var body = require('fs').readFileSync(__dirname + '/fixture.html', 'utf-8')
+
+  it('should minify when specified', function(done) {
+    parse(body, { minify : true },function(err, html) {
+      should.not.exist(err)
+
+      var $ = cheerio.load(html)
+      $('iframe[src*="soundcloud.com"]').length.should.equal(1)
+
+      done()
+    })
+  })
+
+})
