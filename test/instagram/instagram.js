6d9e46df094f15937830a121c702d135fb9d8aeb
Gilles De Mey <gilles.de.mey@gmail.com>
2015-08-03 19:14:37 +0200

    Increased timeout to prevent test from failing under normal conditions

--- test/instagram/instagram.js
+++ test/instagram/instagram.js
@@ -9,6 +9,8 @@ describe('Instagram', function() {
   var body = require('fs').readFileSync(__dirname + '/fixture.html', 'utf-8')
 
   it('should convert to Instagram blockquotes', function(done) {
+    this.timeout(5000)
+
     parse(body, function(err, html) {
       should.not.exist(err)
 
